092719a766d3aba97d5761100c7bff3e8f17451e
Vithya-21 <vithyam2108@gmail.com>
2024-07-28 21:01:26 +0530

    updated the project files

--- python/launch.py
+++ python/launch.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html', message="Hello, World!")
+def hello_world():
+    return 'Hello, User!'
 
 if __name__ == '__main__':
     app.run(debug=True)
